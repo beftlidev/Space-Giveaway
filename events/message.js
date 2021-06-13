7e32a4a2bd1482c28543bfd284ba377f3286a7a4
Glitch (boiled-agreeable-bearberry) <none>
2021-06-13 15:35:01 +0000

    👓🖇 Checkpoint ./events/message.js:19268402/2515

--- events/message.js
+++ events/message.js
@@ -54,42 +54,7 @@ module.exports = async (client, message) => {
   }
     if (cmd) {
       
-      
-      if (db.get(`user_${message.author.id}.şartlar`) != 'kabul edilmiş') {
-     const warningEmbed = new Discord.MessageEmbed()
-    
-     .setColor('RED')
-      .setDescription(`<:codesty_bell:844468553980706816> **Hey! Bu botu kullanabilmek için şartları onaylamanız gerekmektedir.**\n\n<:codesty_support:844468556430704640> **<:rules:799571949286064159>** butonuna basarak botun kurallarına bakabilirsin.`)
-      const termsOfService = new Discord.MessageEmbed()
-      .setColor('RED')
-      .setDescription(`
-<:codesty_join:844468549417697350> **Bir bug vs bulur iseniz lütfen [Destek Sunucumuz](https://discord.gg/KZfAEjrPUF)' da bildirin.**
-<:codesty_join:844468549417697350> **Lütfen bota zarar verecek spam vs gibi şeyler yapmayın.**
-<:codesty_join:844468549417697350> **Botu başka botlar ile lütfen buga sokmayın.**
-<:codesty_join:844468549417697350> **Botta bir hata olur ise lütfen onu botu bozmak vs için kullanmayın.**
-<:codesty_join:844468549417697350> **Lütfen bottaki tüm komutları yavaş kullanın.**
-<:codesty_support:844468556430704640> **Kuralları kabul etmek için <:codesty_check:844468545877442560> basabilir' sin.**
-> <:codesty_link:844468557605240862> [Destek Sunucum](https://discord.gg/KZfAEjrPUF) **|** [Davet et](https://discord.com/oauth2/authorize?client_id=765207268408033322&scope=bot&permissions=805314622) **|** [Oy ver](https://top.gg/bot/765207268408033322) 
-      `)
-      .setTitle(`${client.user.username} **|** Kurallar`);
-            return message.channel.send(warningEmbed).then(async rifleman => {
-        await rifleman.react('<:rules:799571949286064159>');
-        await rifleman.awaitReactions((youth, anasia) => youth.emoji.id == '799571949286064159' && anasia.id == message.author.id, { max: 1 })
-        .then(async () => {
-          await rifleman.reactions.removeAll();
-          await rifleman.edit(termsOfService).then(async leavemealone => {
-            await leavemealone.react('<:codesty_check:844468545877442560>');
-            await leavemealone.awaitReactions((youth, anasia) => youth.emoji.id == '844468545877442560' && anasia.id == message.author.id, { max: 1 })
-            .then(async () => {
-              await leavemealone.reactions.removeAll();
-              await rifleman.edit(termsOfService.setDescription('<:codesty_check:844468545877442560> **Kuralları kabul ettiniz. Artık botu kullanabilirsin!**'));
-              await db.set(`user_${message.author.id}.şartlar`, 'kabul edilmiş');
-            });
-          });
-        });
-      })
-    };
-
+     
     talkedRecently.add(message.author.id);
 
       setTimeout(() => {
