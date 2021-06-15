8b672e7731b97c44641b63f00a1b2e33aa38b0b4
Glitch (boiled-agreeable-bearberry) <none>
2021-06-15 04:13:42 +0000

    ☔️🌂 Checkpoint ./Komutlar/deneme.js:19268402/840

--- Komutlar/deneme.js
+++ Komutlar/deneme.js
@@ -1,20 +1,26 @@
-const Discord = require("discord.js")
+const Discord = require('discord.js');
 
-exports.run = async (client, message, args) => {
+exports.run = async(client, message, args) => {
 
-message.channel.send("Premium üye way canına") 
+var sunucu = client.guilds.cache.get("752164000418234448")
 
-}
+var kişi = sunucu.members.cache.get(message.author.id)
+
+if(!kişi) return message.channel.send("<:codesty_cross:844468546930606100> Bu Komudu kullanabilmek için premium üye olmanız gerekmektedir! \n<:codesty_support:844468556430704640> Detaylı bilgi için [Destek Sunucumuz](https://discord.gg/KZfAEjrPUF)")
+
+  else {
+    message.channel.send("Slm") 
+   } 
+};
 
 exports.conf = {
 
-aliases: [] 
+aliases: []
 
-}
+};
 
 exports.help = {
 
-name: "deneme" 
+name: "osbir"
 
-}
-
+};
