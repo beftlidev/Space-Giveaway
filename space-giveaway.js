e6d5d5e6fc64080dca915b0b078505a6e1f69426
Glitch (pd-cekilis-12) <none>
2021-06-19 08:47:00 +0000

    🍉🎾 Checkpoint ./space-giveaway.js:19268402/299

--- space-giveaway.js
+++ space-giveaway.js
@@ -545,12 +545,3 @@ const discaudio = require("discaudio");
 
         })
 
-const Nuggies = require('nuggies');
-Nuggies.giveaways.connect(process.env.mongoURI);
-client.on('ready', () => {
-    console.log(`${client.user.tag} is online.`)
-    Nuggies.giveaways.startAgain(client);
-});
-client.on('clickButton', button => {
-    Nuggies.giveaways.buttonclick(client, button);
-});
