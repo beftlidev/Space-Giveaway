047db23dda5c8f136b950ddca4ab1e123a5a43b0
Glitch (pd-cekilis-12) <none>
2021-06-20 12:05:58 +0000

    🚄🍥 Checkpoint ./Komutlar/lb.js:19268402/922

---
+++ Komutlar/lb.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js")
+
+const Levels = require("discord-xp") 
+
+exports.run = async (client, message, args) => {
+
+const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10); // We grab top 10 users with most xp in the current server.
+
+if (rawLeaderboard.length < 1) return message.inlineReply("<:carpi:855750448711467058> Henüz kimse tabloda değil!");
+
+const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard, true); // We process the leaderboard.
+
+const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`); // We map the outputs.
+
+const embed = new Discord.MessageEmbed() 
+
+.setDescription(`**Leaderboard**:\n\n${lb.join("\n\n")}`);
+
+message.channel.send(embed) 
+
+} 
+
+exports.conf = {
+
+aliases: [] 
+
+}
+
+exports.help = {
+
+name: "leader-board" 
+
+} 
