2acb9703940204fefad0d3ec59606a724839d68e
Glitch (pd-cekilis-12) <none>
2021-06-20 05:53:46 +0000

    🚥🐿 Checkpoint ./Komutlar/deneme.js:19268402/886

--- Komutlar/deneme.js
+++ Komutlar/deneme.js
@@ -1,18 +1,26 @@
 const Discord = require('discord.js');
-const db = require("croxydb") 
-exports.run = async(client, message, args) => {
-
-// or
+const data = require('croxydb')
+  exports.run = async (client, message, args) => {
+  let açıkmı = await data.fetch(`pre_${message.author.id}`)
+  if(açıkmı) {
+  
+const embed = new Discord.MessageEmbed()
+.setDescription("**Premium üye sin!**")
+.setTimestamp()
+message.channel.send(embed)   
+} else { return message.channel.send(new Discord.MessageEmbed()
+ .setDescription(`Bu komut premiumlulara özel!
+Premium almak için [Destek sunucumuzu](https://discord.gg/KZfAEjrPUF) ziyaret et!`)
+.setTimestamp()
+)
+} 
 };
-
 exports.conf = {
-
-aliases: []
-
-};
-
+  enabled: true,
+  guildOnly: true,
+  aliases: ["test"],
+  permLevel: 0
+}
 exports.help = {
-
-name: "osbir"
-
+  name: 'premiumtest'
 };
