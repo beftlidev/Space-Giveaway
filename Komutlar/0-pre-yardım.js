2fb106c6ae9997207ad924936d5baf507d420aa7
Glitch (pd-cekilis-12) <none>
2021-06-20 07:13:30 +0000

    🏗👶 Checkpoint ./Komutlar/pre-yardım.js:19268402/2853 ./Komutlar/8-yapay-zeka.js:19268402/2576 ./Komutlar/0-yardımm.js:19268402/101

---
+++ Komutlar/0-pre-yardım.js
@@ -0,0 +1,119 @@
+const Discord = require("discord.js")
+
+const disbutpages = require("discord-embeds-pages-buttons")
+
+const disbut = require("discord-buttons") 
+
+exports.run = async (client, message, args) => {
+
+const e1 = new Discord.MessageEmbed() 
+
+.setColor("BLURPLE")
+
+.setAuthor(`Merhaba, ${message.author.username}!`, message.author.avatarURL())
+
+.setTitle("Space Giveaway")
+
+.setURL("https://top.gg/bot/765207268408033322")
+
+.setDescription(`<:emoji_83:847801988656791563> Alttaki butonlara basarak komutları görüntüleyebilirsin!`)
+
+.addField(`<:blurple_tada:841709529217105941> Çekiliş komutlar`, "Galiba sunucuda güzel şeyler olacak sabırsızlıkla bekliyorum!", true)
+
+.addField(`<:emoji_68:841947082076192778> Yetkili komutları`, "Yetkili Komutları ile sunucunuzu daha güzel yapabilir siniz.", true)
+
+.addField("<:emoji_84:847799091886161970> Müzik Komutları", "Biraz müzik dinleyelim 🎶") 
+
+.addField("<:tembel:802431611848228904> Eğlence Komutları", "Biraz eğlenmek herkesin hakkı ;D") 
+
+.addField(`<:game:842658421231845408> Oyun Komutları`, "Oyun komutlarım bence bir harika!!", true)
+
+const e2 = new Discord.MessageEmbed() 
+
+.setColor("GREEN")
+
+.setAuthor(`Merhaba, ${message.author.username}!`, message.author.avatarURL())
+
+.setTitle("<:blurple_tada:841709529217105941> Çekiliş Komutları")
+
+.setURL("https://top.gg/bot/765207268408033322")
+
+.setDescription(`Yakında!
+
+`) 
+
+const e3 = new Discord.MessageEmbed() 
+
+.setColor("GREEN") 
+
+.setAuthor(`Merhaba, ${message.author.username}!`, message.author.avatarURL())
+
+.setTitle("<:emoji_68:841947082076192778> Yetkili Komutları")
+
+.setURL("https://top.gg/bot/765207268408033322")
+
+.setDescription(`> **g.yapay-zeka aç** - Yapay zeka açarsınız. 
+
+> **g.yapay-zeka kapat** - Yapay zeka kapatırsınız.
+
+`) 
+
+const e4 = new Discord.MessageEmbed() 
+
+.setColor("GREEN") 
+
+.setAuthor(`Merhaba, ${message.author.username}!`, message.author.avatarURL())
+
+.setTitle("<:emoji_84:847799091886161970> Müzik Komutları")
+
+.setURL("https://top.gg/bot/765207268408033322")
+
+.setDescription(`Yakında!
+
+`) 
+
+const e5 = new Discord.MessageEmbed() 
+
+.setColor("GREEN") 
+
+.setAuthor(`Merhaba, ${message.author.username}!`, message.author.avatarURL())
+
+.setTitle("<:tembel:802431611848228904> Eğlence Komutları")
+
+.setURL("https://top.gg/bot/765207268408033322")
+
+.setDescription(`Yakında! 
+
+`) 
+
+const e6 = new Discord.MessageEmbed() 
+
+.setColor("GREEN") 
+
+.setAuthor(`Merhaba, ${message.author.username}!`, message.author.avatarURL())
+
+.setTitle("<:game:842658421231845408> Oyun Komutları")
+
+.setURL("https://top.gg/bot/765207268408033322")
+
+.setDescription(`Yakında!
+
+`) 
+
+var pages = [e1, e2, e3, e4, e5, e6]
+
+disbutpages.pages(client, message, pages, 300000, disbut, "red", "844468549417697350", "844468548079321089", "844468546930606100")
+
+} 
+
+exports.conf = {
+
+aliases: [] 
+
+}
+
+exports.help = {
+
+name: "pre-yardım" 
+
+}  
--- Komutlar/0-yardımm.js
+++ Komutlar/0-yardımm.js
@@ -108,8 +108,6 @@ exports.run = async (client, message, args) => {
 > **g.yedek oluştur** - Yedek oluşturursunuz.
 > **g.yedek bilgi** - Oluşturduğunuz yedek hakkında bilgi alırsınız. 
 > **g.yedek yükle** - Oluşturdupunuz yedeği geri yüklersiniz.
-> **g.yapay-zeka aç** - Yapay zeka açarsınız. 
-> **g.yapay-zeka kapat** - Yapay zeka kapatırsınız.
 `) 
    const embed5 = new Discord.MessageEmbed() 
 
